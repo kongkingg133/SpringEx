86c87fcbff1da56e1d7da908219f82920d59c2d3
kcas1005 <wltjdwls9@naver.com>
2022-08-04 11:51:29 +0900

    Day57_get방식 Post방식 requestMapping

--- src/main/java/com/human/pet/Board.java
+++ src/main/java/com/human/pet/Board.java
@@ -1,11 +1,15 @@
 package com.human.pet;
 
+//외장 라이브러리
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Date;
-
+//롬북에 있는 Getter라는 메서드를 통해
+//하단에 있는 클래스 board는
+//자동으로 getter, setter 메서드가 생성됨을
+//암시함(@어노테이션)
 @Getter
 @Setter
 @ToString
@@ -17,4 +21,7 @@ public class Board {
     private String content;
     private Date createDate;
     private Long cnt;
+    
+    //원래는 setter, getter라는 메서드가 있어야 private 필드값에 데이터를 넣을 수 있지만,
+    //(gradle에서 설치)롬북 이라는 라이브러리로 자동 getter, setter 메서드 생성
 }
--- src/main/java/com/human/pet/controller/BoardController.java
+++ src/main/java/com/human/pet/controller/BoardController.java
@@ -1,16 +1,24 @@
 package com.human.pet.controller;
 
+//외장 라이브러리 호출(import), gradle로 설치한 라이브러리
 import com.human.pet.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//내장 라이브러리 호출(import)
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Controller
 public class BoardController {
+
+//    @RequestMapping은 서버에서 디스페처서블릿을 통해 html의 action태그의 주소와 동일한
+//    문자열을 찾는 매핑기능(연결)이 실행되고 하단에 메서드가 실행
+//    return String인 이유는 뷰리졸버가 html파일을 찾기 위한 문자열 리턴
 
     //클라이언트에서 서버로 무언가 데이터를 전송하기 위한 Mapping(@RequestMapping)
     @RequestMapping("/getBoardList")
@@ -27,16 +35,44 @@ public class BoardController {
             board.setSeq(new Long(i));
             board.setTitle("게시판 프로그램 테스트"+i);
             board.setWriter("도우너");
-            board.setContent("게시판 프로그램 테스트입니다...");
+            board.setContent("게시판 프로그램 테스트입니다.");
+            //내장 클래스인 java.util.Date 객체로 시간 데이터 출력
             board.setCreateDate(new Date());
             board.setCnt(0L);
             //boardList배열에 board객체 넣기(for문 10번 도니까 board객체 10개 넣기)
             boardList.add(board);
         }
+        //model 객체에 boardList(arrayList)를 boardList key값으로 넣음
+        //attributeName = key
+        //attributeValue = value
+        //model에는 참조타입만 넣을 수 있다(addAttribute 메서드 안에 매개변수 타입으로 확인 가능)
         model.addAttribute("boardList", boardList);
         System.out.println("출력");
+        //디서패처서블릿이 뷰 리졸버를 찾아서 연결해 줍니다.
         //viewResolver
         //return getBoardList라는 문자열로 templates에 있는 같은 이름에 html파일을 찾는다.
         return "getBoardList";
         }
+        @GetMapping("/insertBoard")
+        public String insertBoardView(){
+            return "insertBoard";
+        }
+        @GetMapping("/getBoard")
+        public String getBoard(Board board, Model model){
+            model.addAttribute("board"/*, boardService.getBoard(board)*/);
+            return "getBoard";
+        }
+        @PostMapping("updateBoard")
+    public String updateBoard(Board board){
+        /*boardService.updateBoard(board);*/
+        return "foward:getBoardList";
+        }
+
+
+
+//        @PostMapping("/insertBoard")
+//        public String insertBoard(Board board){
+//            boardService.insertBoard(board);
+//            return "redirect:getBoardList";
+//        }
     }
